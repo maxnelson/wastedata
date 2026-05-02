35a8e084c31ca25f6c2f90101ba49ba58868f0c6
Max Nelson <maxnelson88@gmail.com>
2026-05-02 03:00:10 -0700

    fixing up footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,67 +1,72 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons'
+import nyanCat from '../../assets/nyan-cat.png'
 import styles from './Footer.module.css'
+
+function ExtIcon() {
+  return <FontAwesomeIcon icon={faUpRightFromSquare} className={styles.extIcon} />
+}
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
 
-      <div className={styles.columns}>
+      <div className={styles.inner}>
 
-        <div className={styles.col}>
-          <h4 className={styles.colHeading}>Project</h4>
-          <ul className={styles.colList}>
-            <li><a href="/about" className={styles.colLink}>About the Data</a></li>
-            <li>
-              <a
-                href="#portfolio"
-                className={styles.colLink}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Portfolio
-              </a>
-            </li>
-          </ul>
+        <div className={styles.columns}>
+
+          <div className={styles.col}>
+            <h4 className={styles.colHeading}>Project</h4>
+            <ul className={styles.colList}>
+              <li><a href="/about" className={styles.colLink}>About the Data</a></li>
+              <li>
+                <a href="#donate" className={styles.colLink}>
+                  Buy me a coffee :)
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.col}>
+            <h4 className={styles.colHeading}>Data Sources</h4>
+            <ul className={styles.colList}>
+              <li>
+                <a
+                  href="https://www2.calrecycle.ca.gov/WasteCharacterization/"
+                  className={styles.colLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  CalRecycle <ExtIcon />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://dof.ca.gov/forecasting/demographics/estimates/"
+                  className={styles.colLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  CA Dept. of Finance <ExtIcon />
+                </a>
+              </li>
+            </ul>
+          </div>
+
         </div>
 
-        <div className={styles.col}>
-          <h4 className={styles.colHeading}>Data Sources</h4>
-          <ul className={styles.colList}>
-            <li>
-              <a
-                href="https://www2.calrecycle.ca.gov/WasteCharacterization/"
-                className={styles.colLink}
-                target="_blank"
-                rel="noreferrer"
-              >
-                CalRecycle
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://dof.ca.gov/forecasting/demographics/estimates/"
-                className={styles.colLink}
-                target="_blank"
-                rel="noreferrer"
-              >
-                CA Dept. of Finance
-              </a>
-            </li>
-            <li className={styles.colNote}>Characterization figures are estimates, not direct measurements.</li>
-          </ul>
-        </div>
-
-        <div className={styles.col}>
-          <h4 className={styles.colHeading}>Support</h4>
-          <ul className={styles.colList}>
-            <li>
-              <a href="#donate" className={styles.donateBtn}>
-                Donate
-              </a>
-            </li>
-          </ul>
-        </div>
+        <a
+          href="https://maxnelsonwebsite.com/"
+          className={styles.nyanLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={nyanCat} alt="Nyan Cat" className={styles.nyanCat} />
+        </a>
 
       </div>
+
+      <p className={styles.disclaimer}>Characterization figures are estimates, not direct measurements.</p>
 
       <div className={styles.copyright}>
         <p>© {new Date().getFullYear()} WasteData CA</p>
