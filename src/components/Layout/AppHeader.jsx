6262ff753aa4e6603ecbc5bc506ef1af9ca6eae7
Max Nelson <maxnelson88@gmail.com>
2026-05-02 00:23:36 -0700

    fixed up stuff for deployment

--- src/components/Layout/AppHeader.jsx
+++ src/components/Layout/AppHeader.jsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashCan } from '@fortawesome/pro-regular-svg-icons'
-import styles from './AppHeader.module.css'
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/pro-regular-svg-icons";
+import styles from "./AppHeader.module.css";
 
 export default function AppHeader() {
   return (
     <header className={styles.header}>
       <Link to="/" className={styles.logo}>
         <FontAwesomeIcon icon={faTrashCan} className={styles.logoMark} />
-        <span className={styles.logoName}>Trashdata</span>
+        <span className={styles.logoName}>Wastedata&nbsp;</span>
       </Link>
     </header>
-  )
+  );
 }
